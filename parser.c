0329ada641e11e26d1e033d4e4e3991aad43d214
brunoczim <brunoczim@gmail.com>
2020-06-22 19:12:56 -0300

    implementado logaritmo

--- parser.c
+++ parser.c
@@ -74,6 +74,10 @@ static int parse_op(struct parser *parser, enum operation *output)
         *output = op_pow;
     } else if (advance_op(parser, OP_EXP_SYM, sizeof(OP_EXP_SYM))) {
         *output = op_exp;
+    } else if (advance_op(parser, OP_LOG_SYM, sizeof(OP_LOG_SYM))) {
+        *output = op_log;
+    } else if (advance_op(parser, OP_LN_SYM, sizeof(OP_LN_SYM))) {
+        *output = op_ln;
     } else {
         success = 0;
     }
